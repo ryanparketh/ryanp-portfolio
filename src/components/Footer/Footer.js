f717e3285cfb5b603438c23873e9779ebd3c9897
ryanparketh <ryanpark@rocketmail.com>
2023-03-10 11:35:40 -0500

    fixed footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rajshekhar26/cleanfolio'
+      href='https://github.com/ryanparketh/ryanp-portfolio'
       className='link footer__link'
     >
       Created By Ryan Park
